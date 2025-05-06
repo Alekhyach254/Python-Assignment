c77cb99a9f025a2109bb875f049841a4827b0b20
Alekhyach254 <alekhyach254@gmail.com>
2025-05-06 13:47:08 -0700

    Update Week_1.py

--- Week_1.py
+++ Week_1.py
@@ -3,4 +3,4 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 height = input("Enter your height in feet: ")
 
-print(f"My name is {name}, I am {age} years old, and I am {height} feet tall.")
+print(f"My name is {name}, I am {age} years old, and I am {height} feet cm.")
