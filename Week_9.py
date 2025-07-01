298a13928aa2fbb4d59560278ae29cbd90116f1b
Alekhyach254 <alekhyach254@gmail.com>
2025-07-01 12:10:16 -0700

    Create Week_9.py

---
+++ Week_9.py
@@ -0,0 +1,6 @@
+def square_number(n):
+    """Returns the square of the input number."""
+    return n ** 2
+print("Square of 4 is:", square_number(4))     
+print("Square of 10 is:", square_number(10))   
+print("Square of 8 is:", square_number(8)) 
